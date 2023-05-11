63042dd6c7aaf77970b9084d85294c05d8a3e228
katomizuki <katobad.0405@gmail.com>
2023-05-11 23:26:41 +0900

    add feature

--- Assets/Scripts/ToucSample.cs
+++ Assets/Scripts/ToucSample.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class ToucSample : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private XRBaseControllerInteractor _interactor;
+    private void OnEnable()
     {
-        
+        _interactor.hoverEntered.AddListener(DebugPrint);
+        _interactor.hoverExited.AddListener(DebugPrint);
+        _interactor.selectEntered.AddListener(DebugPrint);
+        _interactor.selectExited.AddListener(DebugPrint);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _interactor.hoverEntered.RemoveListener(DebugPrint);
+        _interactor.hoverExited.RemoveListener(DebugPrint);
+        _interactor.selectEntered.RemoveListener(DebugPrint);
+        _interactor.selectExited.RemoveListener(DebugPrint);
+    }
+
+    void DebugPrint(BaseInteractionEventArgs args)
+    {
+        Debug.Log(args.interactable.name + " " + args.interactor.name + " " + args.GetType().Name);
     }
 }
